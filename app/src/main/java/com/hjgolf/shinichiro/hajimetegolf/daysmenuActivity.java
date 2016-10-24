959654ac5c0d37714153d8eba396ddbe33761105
IMAC <imac@IMAC-no-iMac.local>
2016-10-24 09:10:48 +0900

    Initial commit

---
+++ app/src/main/java/com/hjgolf/shinichiro/hajimetegolf/daysmenuActivity.java
@@ -0,0 +1,13 @@
+package com.hjgolf.shinichiro.hajimetegolf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class daysmenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daysmen);
+    }
+}
